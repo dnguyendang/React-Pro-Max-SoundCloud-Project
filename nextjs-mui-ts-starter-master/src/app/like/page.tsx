41d760b2c62842a506eb6b3634495b6e70e03744
dungduongthanhson <dungduongthanhson@gmail.com>
2025-09-25 17:08:21 +0700

    update: header

---
+++ nextjs-mui-ts-starter-master/src/app/like/page.tsx
@@ -0,0 +1,14 @@
+'use client'
+import { useState } from "react";
+
+const LikePage = () => {
+    const [name, setName] = useState("Dung ND")
+
+    return (
+        <div>
+            like page
+        </div>
+    )
+}
+
+export default LikePage;
--- nextjs-mui-ts-starter-master/src/app/page.tsx
+++ nextjs-mui-ts-starter-master/src/app/page.tsx
@@ -1,5 +1,10 @@
+import AppHeader from "@/components/header/app.header";
+
 export default function HomePage() {
   return (
-    <div>hello world</div>
+    <div>
+      homepage
+
+    </div>
   );
 }
