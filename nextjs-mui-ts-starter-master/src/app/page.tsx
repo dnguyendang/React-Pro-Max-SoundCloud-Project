7c80670ab1f6c06a36db8b654b261f0a84c43fb5
dungduongthanhson <dungduongthanhson@gmail.com>
2025-09-26 17:17:06 +0700

    update: footer

--- nextjs-mui-ts-starter-master/src/app/page.tsx
+++ nextjs-mui-ts-starter-master/src/app/page.tsx
@@ -1,10 +1,25 @@
-import AppHeader from "@/components/header/app.header";
+import MainSlider from "@/components/main/main.slider";
+import { Container } from "@mui/material"
 
-export default function HomePage() {
+export default async function HomePage() {
+  const res = await fetch("http://localhost:8000/api/v1/tracks/top", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      category: "CHILL",
+      limit: 2
+    })
+  })
+
+  console.log(">>> check res server: ", res.json())
+
   return (
-    <div>
-      homepage
-
-    </div>
+    <Container>
+      <MainSlider />
+      <MainSlider />
+      <MainSlider />
+    </Container>
   );
 }
